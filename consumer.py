100567d86458ddb97c873f9600b8c78301253f61
raymelguerra <raymelguerra@gmail.com>
2023-04-27 16:01:37 -0400

    any chnages

--- consumer.py
+++ consumer.py
@@ -2,35 +2,36 @@ from threading import Thread
 import pika
 import json
 from helpers.smsc_helper import SMSCHelper
-from dotenv import load_dotenv
 import os
+import logging
+import time
+import settings
+import traceback
 
-load_dotenv()
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
-credentials = pika.PlainCredentials(os.getenv("RABBIT_CREDENTIALS_USER"),os.getenv("RABBIT_CREDENTIALS_PASSWORD"))
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=os.getenv("RABBIT_CREDENTIALS_HOST"), port=os.getenv("RABBIT_CREDENTIALS_PORT"), credentials= credentials))
-# credentials = pika.PlainCredentials(RABBIT_CREDENTIALS_USER, RABBIT_CREDENTIALS_PASSWORD)
-# connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBIT_CREDENTIALS_HOST, port=RABBIT_CREDENTIALS_PORT, credentials= credentials))
+
+credentials = pika.PlainCredentials(settings.RABBIT_CREDENTIALS_USER, settings.RABBIT_CREDENTIALS_PASSWORD)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBIT_CREDENTIALS_HOST, port=settings.RABBIT_CREDENTIALS_PORT, credentials= credentials))
 channel = connection.channel()
 channel.exchange_declare('send-sms-topic', durable=True, exchange_type='topic')
 
 def send_sms(ch,method,properties,body):
+    logging.info('send poll sms')
     data = json.loads(body.decode('utf-8'))
-    connection = SMSCHelper(host=os.getenv("SMS_HOST_USER"), port=os.getenv("SMS_HOST_PORT"),
-                               system_id=os.getenv("SMS_HOST_SYSTEM_ID"),
-                               password=os.getenv("SMS_HOST_PASSWORD"), source_number=data['data']['payload']['source'])
-    # connection = SMSCHelper(host=SMS_HOST_USER, port=SMS_HOST_PORT,
-    #                            system_id=SMS_HOST_SYSTEM_ID,
-    #                            password= SMS_HOST_PASSWORD, source_number=data['data']['payload']['source'])
+    connection = SMSCHelper(host=settings.SMS_HOST_USER, port=settings.SMS_HOST_PORT,
+                               system_id=settings.SMS_HOST_SYSTEM_ID,
+                               password=settings.SMS_HOST_PASSWORD, source_number=data['data']['payload']['source'])
     index = 0
     for item in data['data']['payload']['destinations']:
-        _ =  connection.send_short_message(message=item['body'], destination_number=item['destination'])
-        print(f'{index}. Send message. To: {item} Body: {data["data"]["payload"]["body"]} From: {data["data"]["payload"]["source"]}')
+        _ =  connection.send_short_message(message=data['data']['payload']['body'], destination_number=item)
+        logging.info(f'{index}. Send message. To: {item} Body: {data["data"]["payload"]["body"]} From: {data["data"]["payload"]["source"]}')
         index = index + 1
     
 
 def startConsumer():
-    connection= pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    connection= pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBIT_CREDENTIALS_HOST))
+    logging.info('Consumer is connected')
     #creating channels
     channel= connection.channel()
     #connecting queues to channels
@@ -45,4 +46,6 @@ while True:
         if Exception is KeyboardInterrupt:
             break
         else:
+            print(traceback.format_exc())
+            time.sleep(1)
             continue
